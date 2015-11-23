1f53ecf3688048098a99bd92ead0a71edff12511
Meena <meena@multicorewareinc.com>
2015-11-23 18:30:22 +0530

    Add Macros needed for csrmv routines

--- source/csrmv.hpp
+++ source/csrmv.hpp
@@ -1,5 +1,47 @@
 #include "hcsparse.h"
 
+#define WAVE_SIZE 64
+#define SUBWAVE_SIZE 64
+#define WG_SIZE 256
+#define INDEX_TYPE int
+#define SIZE_TYPE ulong
+#define VALUE_TYPE float
+#define GLOBAL_SIZE WG_SIZE
+#define EXTENDED_PRECISION 1
+#define ROWS_FOR_VECTOR 1
+#define BLOCK_MULTIPLIER 3
+#define BLOCKSIZE 1024
+#define WGBITS 24
+#define ROWBITS 32
+
+#ifndef INDEX_TYPE
+#error "INDEX_TYPE undefined!"
+#endif
+
+#ifndef VALUE_TYPE
+#error "VALUE_TYPE undefined!"
+#endif
+
+#ifndef SIZE_TYPE
+#error "SIZE_TYPE undefined!"
+#endif
+
+#ifndef WG_SIZE
+#error "WG_SIZE undefined!"
+#endif
+
+#ifndef WAVE_SIZE
+#error "WAVE_SIZE undefined!"
+#endif
+
+#ifndef SUBWAVE_SIZE
+#error "SUBWAVE_SIZE undefined!"
+#endif
+
+#if ( (SUBWAVE_SIZE > WAVE_SIZE) || (SUBWAVE_SIZE != 2 && SUBWAVE_SIZE != 4 && SUBWAVE_SIZE != 8 && SUBWAVE_SIZE != 16 && SUBWAVE_SIZE != 32 && SUBWAVE_SIZE != 64) )
+#error "SUBWAVE_SIZE is not  a power of two!"
+#endif
+
 template<typename T>
 hcsparseStatus
 csrmv_vector(const hcsparseScalar* pAlpha,
