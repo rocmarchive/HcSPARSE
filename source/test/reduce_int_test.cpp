a26ca2b0ad26858391460a7d960548df3d05ca58
Yuvaraj Venkatesh <yuvaraj@multicorewareinc.com>
2015-12-12 05:10:35 -0600

    Add test file to test hcdenseIreduce

---
+++ source/test/reduce_int_test.cpp
@@ -0,0 +1,60 @@
+#include <hcsparse.h>
+#include <iostream>
+int main()
+{
+    hcsparseScalar gR;
+    hcdenseVector gX;
+
+    std::vector<Concurrency::accelerator>acc = Concurrency::accelerator::get_all();
+    accelerator_view accl_view = (acc[1].create_view()); 
+
+    hcsparseControl control(accl_view);
+
+    int num_elements = 100;
+    int *host_res = (int*) calloc(1, sizeof(int));
+    int *host_X = (int*) calloc(num_elements, sizeof(int));
+    int *host_R = (int*) calloc(1, sizeof(int));
+
+    srand (time(NULL));
+    for (int i = 0; i < num_elements; i++)
+    {
+        host_X[i] = rand()%100;
+    }
+    
+    Concurrency::array_view<int> dev_X(num_elements, host_X);
+    Concurrency::array_view<int> dev_R(1, host_R);
+
+    hcsparseSetup();
+    hcsparseInitScalar(&gR);
+    hcsparseInitVector(&gX);
+
+    gR.value = &dev_R;
+    gX.values = &dev_X;
+
+    gR.offValue = 0;
+    gX.offValues = 0;
+
+    gX.num_values = num_elements;
+
+    hcsparseStatus status;
+
+    status = hcdenseIreduce(&gR, &gX, &control);
+
+    for (int i = 0; i < num_elements; i++)
+    {
+        host_res[0] += host_X[i];
+    }
+
+    bool ispassed = 1;
+    Concurrency::array_view<int> *av_res = static_cast<Concurrency::array_view<int> *>(gR.value);
+    if (host_res[0] != (*av_res)[0])
+    {
+        ispassed = 0;
+    }
+
+    std::cout << (ispassed?"TEST PASSED":"TEST FAILED") << std::endl;
+
+    hcsparseTeardown();
+
+    return 0; 
+}
