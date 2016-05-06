d7fb46f69e93f03729a23386134caf9468939967
Yuvaraj Venkatesh <yuvaraj@multicorewareinc.com>
2016-05-06 16:25:17 +0530

    Restructure gtest and unit test dir

--- cmake/FindHC++.cmake
+++ cmake/FindHC++.cmake
@@ -15,42 +15,11 @@ if( MSVC OR APPLE)
   message(FATAL_ERROR "Unsupported platform.")
 endif()
 
+
 set(MCWHCCBUILD $ENV{MCWHCCBUILD})
 # Package built from sources
 # Compiler and configure file are two key factors to advance
-if(EXISTS /opt/hcc/bin/clang++)
-  find_path(HC++_BIN_DIR clang++
-           HINTS /opt/hcc/bin)
-  find_path(HC++_CONFIGURE_DIR hcc-config
-           HINTS /opt/hcc/bin)
-  include(FindPackageHandleStandardArgs)
-  # handle the QUIETLY and REQUIRED arguments and set HC++_FOUND to TRUE
-  # if all listed variables are TRUE
-  find_package_handle_standard_args(HC++  DEFAULT_MSG
-                                    HC++_BIN_DIR HC++_CONFIGURE_DIR)
-  mark_as_advanced(HC++_BIN_DIR HC++_CONFIGURE_DIR)
-  if (HC++_FOUND)
-    message(STATUS "HC++ Compiler found in ${HC++_BIN_DIR}/..")
-    set(CMAKE_C_COMPILER ${HC++_BIN_DIR}/clang)
-    set(CMAKE_CXX_COMPILER ${HC++_BIN_DIR}/clang++)
-  elseif()
-    message(FATAL_ERROR "HC++ Compiler not found.")
-  endif()
-
-  # Build mode
-  set (CLANG_AMP "${HC++_BIN_DIR}/clang++")
-  set (HCC_CONFIG "${HC++_CONFIGURE_DIR}/hcc-config")
-  execute_process(COMMAND ${HCC_CONFIG} --build --cxxflags
-                  OUTPUT_VARIABLE HCC_CXXFLAGS)
-  string(STRIP "${HCC_CXXFLAGS}" HCC_CXXFLAGS)
-  set (HCC_CXXFLAGS "${HCC_CXXFLAGS}")
-  execute_process(COMMAND ${HCC_CONFIG} --build --ldflags --shared
-                  OUTPUT_VARIABLE HCC_LDFLAGS)
-  string(STRIP "${HCC_LDFLAGS}" HCC_LDFLAGS)
-  set (HCC_CXXFLAGS "${HCC_CXXFLAGS} -Wall -Wno-deprecated-register -Wno-deprecated-declarations")
-  set (HCC_LDFLAGS "${HCC_LDFLAGS}")
-
-elseif(EXISTS ${MCWHCCBUILD})
+if(EXISTS ${MCWHCCBUILD})
   find_path(HC++_BIN_DIR clang++
            HINTS ${MCWHCCBUILD}/compiler/bin)
   find_path(HC++_CONFIGURE_DIR hcc-config
@@ -72,16 +41,47 @@ elseif(EXISTS ${MCWHCCBUILD})
   # Build mode
   set (CLANG_AMP "${HC++_BIN_DIR}/clang++")
   set (HCC_CONFIG "${HC++_CONFIGURE_DIR}/hcc-config")
-  execute_process(COMMAND ${HCC_CONFIG} --build --cxxflags
+  execute_process(COMMAND ${HCC_CONFIG} --build --bolt --cxxflags
                   OUTPUT_VARIABLE HCC_CXXFLAGS)
   string(STRIP "${HCC_CXXFLAGS}" HCC_CXXFLAGS)
   set (HCC_CXXFLAGS "${HCC_CXXFLAGS}")
-  execute_process(COMMAND ${HCC_CONFIG} --build --ldflags --shared
+  execute_process(COMMAND ${HCC_CONFIG} --build --bolt --ldflags --shared
                   OUTPUT_VARIABLE HCC_LDFLAGS)
   string(STRIP "${HCC_LDFLAGS}" HCC_LDFLAGS)
   set (HCC_CXXFLAGS "${HCC_CXXFLAGS} -Wall -Wno-deprecated-register -Wno-deprecated-declarations")
   set (HCC_LDFLAGS "${HCC_LDFLAGS}")
 
+elseif(EXISTS /opt/rocm/hcc/bin/clang++)
+  find_path(HC++_BIN_DIR clang++
+           HINTS /opt/rocm/hcc/bin)
+  find_path(HC++_CONFIGURE_DIR hcc-config
+           HINTS /opt/rocm/hcc/bin)
+  include(FindPackageHandleStandardArgs)
+  # handle the QUIETLY and REQUIRED arguments and set HC++_FOUND to TRUE
+  # if all listed variables are TRUE
+  find_package_handle_standard_args(HC++  DEFAULT_MSG
+                                    HC++_BIN_DIR HC++_CONFIGURE_DIR)
+  mark_as_advanced(HC++_BIN_DIR HC++_CONFIGURE_DIR)
+  if (HC++_FOUND)
+    message(STATUS "HC++ Compiler found in ${HC++_BIN_DIR}/..")
+    set(CMAKE_C_COMPILER ${HC++_BIN_DIR}/clang)
+    set(CMAKE_CXX_COMPILER ${HC++_BIN_DIR}/clang++)
+  elseif()
+    message(FATAL_ERROR "HC++ Compiler not found.")
+  endif()
+
+  # Build mode
+  set (CLANG_AMP "${HC++_BIN_DIR}/clang++")
+  set (HCC_CONFIG "${HC++_CONFIGURE_DIR}/hcc-config")
+  execute_process(COMMAND ${HCC_CONFIG} --bolt --cxxflags
+                  OUTPUT_VARIABLE HCC_CXXFLAGS)
+  string(STRIP "${HCC_CXXFLAGS}" HCC_CXXFLAGS)
+  set (HCC_CXXFLAGS "${HCC_CXXFLAGS}")
+  execute_process(COMMAND ${HCC_CONFIG} --bolt --ldflags --shared
+                  OUTPUT_VARIABLE HCC_LDFLAGS)
+  string(STRIP "${HCC_LDFLAGS}" HCC_LDFLAGS)
+  set (HCC_CXXFLAGS "${HCC_CXXFLAGS} -Wall -Wno-deprecated-register -Wno-deprecated-declarations")
+  set (HCC_LDFLAGS "${HCC_LDFLAGS}")
+
+
 endif()
-
-
---
+++ install.sh
@@ -0,0 +1,132 @@
+#!/bin/bash -e
+#This script is invoked to install the hcsparse library and test sources
+
+# CHECK FOR COMPILER PATH
+
+if [ ! -z $MCWHCCBUILD ];
+then
+  if [ -x "$MCWHCCBUILD/compiler/bin/clang++" ];
+  then
+    cmake_c_compiler="$MCWHCCBUILD/compiler/bin/clang"
+    cmake_cxx_compiler="$MCWHCCBUILD/compiler/bin/clang++"
+  fi
+
+elif [ -x "/opt/rocm/hcc/bin/clang++" ];
+then
+  cmake_c_compiler="/opt/rocm/hcc/bin/clang"
+  cmake_cxx_compiler="/opt/rocm/hcc/bin/clang++"
+else
+  echo "Clang compiler not found"
+  exit 1
+fi
+
+#CURRENT_WORK_DIRECTORY
+current_work_dir=$PWD
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# Help menu
+print_help() {
+cat <<-HELP
+=============================================================================================================================
+This script is invoked to install hcblas library and test sources. Please provide the following arguments:
+
+  1) ${green}--path${reset}    Path to your hcblas installation.(default path is /opt/ROCm/ - needs sudo access)
+  2) ${green}--test${reset}    Test to enable the library testing. 
+
+=============================================================================================================================
+Usage: ./install.sh --path=/path/to/user/installation --test=on
+=============================================================================================================================
+Example: 
+(1) ${green}./install.sh --path=/path/to/user/installation --test=on 
+       <library gets installed in /path/to/user/installation, testing = on>
+(2) ${green}./install.sh --test=on (sudo access needed)
+       <library gets installed in /opt/ROCm/, testing = on>
+
+=============================================================================================================================
+HELP
+exit 0
+}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --path=*)
+      path="${1#*=}"
+      ;;
+    --test=*)
+      testing="${1#*=}"
+      ;;
+    --help) print_help;;
+    *)
+      printf "************************************************************\n"
+      printf "* Error: Invalid arguments, run --help for valid arguments.*\n"
+      printf "************************************************************\n"
+      exit 1
+  esac
+  shift
+done
+
+if [ -z $path ]; then
+    path="/opt/ROCm/"
+fi
+
+if [ "$path" = "/opt/ROCm/" ]; then
+   set +e
+   sudo mkdir /opt/ROCm/
+   set -e
+fi
+
+export hcsparse_install=$path
+set hcsparse_install=$path
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$hcsparse_install/lib/hcsparse
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$hcsparse_install/include/hcsparse
+export CLAMP_NOTILECHECK=ON
+
+set +e
+# MAKE BUILD DIR
+mkdir $current_work_dir/build
+mkdir $current_work_dir/build/test
+set -e
+
+# SET BUILD DIR
+build_dir=$current_work_dir/build
+
+# change to library build
+cd $build_dir
+
+# Cmake and make libhcblas: Install hcblas
+cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC $current_work_dir
+if [ "$path" = "/opt/ROCm/" ]; then
+    sudo -E make install
+else
+    make install
+fi
+
+# Test=OFF (Build library and tests)
+if ( [ -z $testing ] ) || ( [ "$testing" = "off" ] ); then
+  echo "${green}HCSPARSE Installation Completed!${reset}"
+# Test=ON (Build and test the library)
+elif ( [ "$testing" = "on" ] ); then
+ # Build Tests
+   cd $build_dir/test/ && cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC $current_work_dir/test/
+   set +e
+   mkdir $current_work_dir/build/test/unit_test/bin/
+   mkdir $current_work_dir/build/test/gtest/bin/
+   set -e
+   make
+
+#  cd $current_work_dir/build/test/gtest/bin/
+#  ./unittest
+fi
+
+if grep --quiet hcsparse ~/.bashrc; then
+  echo 
+else
+  eval "echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH' >> ~/.bashrc"
+fi
+
+cd $current_work_dir
+exec bash  
