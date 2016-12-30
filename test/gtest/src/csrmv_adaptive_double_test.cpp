1b33a7d5030e4332248eaf86dfa7b8f159eae0ba
Sujitha <sujitha@multicorewareinc.com>
2016-12-30 13:29:55 +0530

    Using hc_am functions in the csrmv_adaptive and csrmv routines

--- test/gtest/src/csrmv_adaptive_double_test.cpp
+++ test/gtest/src/csrmv_adaptive_double_test.cpp
@@ -1,5 +1,6 @@
 #include <hcsparse.h>
 #include <iostream>
+#include <hc_am.hpp>
 #include "gtest/gtest.h"
 
 TEST(csrmv_adaptive_double_test, func_check)
@@ -15,11 +16,18 @@ TEST(csrmv_adaptive_double_test, func_check)
 
     hcsparseControl control(accl_view);
 
-    const char* filename = "../../../src/input.mtx";
+    const char* filename = "./../../../../test/gtest/src/input.mtx";
 
     int num_nonzero, num_row, num_col;
 
     hcsparseStatus status;
+    hcsparseSetup();
+    hcsparseInitCsrMatrix(&gCsrMat);
+    hcsparseInitScalar(&gAlpha);
+    hcsparseInitScalar(&gBeta);
+    hcsparseInitVector(&gX);
+    hcsparseInitVector(&gY);
+
 
     status = hcsparseHeaderfromFile(&num_nonzero, &num_row, &num_col, filename);
 
@@ -49,22 +57,15 @@ TEST(csrmv_adaptive_double_test, func_check)
     host_alpha[0] = 1;
     host_beta[0] = 0;
 
-    array_view<double> dev_X(num_col, host_X);
-    array_view<double> dev_Y(num_row, host_Y);
-    array_view<double> dev_alpha(1, host_alpha);
-    array_view<double> dev_beta(1, host_beta);
+    gX.values = am_alloc(sizeof(double) * num_col, acc[1], 0);
+    gY.values = am_alloc(sizeof(double) * num_row, acc[1], 0);
+    gAlpha.value = am_alloc(sizeof(double) * 1, acc[1], 0);
+    gBeta.value = am_alloc(sizeof(double) * 1, acc[1], 0);
 
-    hcsparseSetup();
-    hcsparseInitCsrMatrix(&gCsrMat);
-    hcsparseInitScalar(&gAlpha);
-    hcsparseInitScalar(&gBeta);
-    hcsparseInitVector(&gX);
-    hcsparseInitVector(&gY);
-
-    gAlpha.value = &dev_alpha;
-    gBeta.value = &dev_beta;
-    gX.values = &dev_X;
-    gY.values = &dev_Y;
+    control.accl_view.copy(host_X, gX.values, sizeof(double) * num_col);
+    control.accl_view.copy(host_Y, gY.values, sizeof(double) * num_row);
+    control.accl_view.copy(host_alpha, gAlpha.value, sizeof(double) * 1);
+    control.accl_view.copy(host_beta, gBeta.value, sizeof(double) * 1);
 
     gAlpha.offValue = 0;
     gBeta.offValue = 0;
@@ -79,27 +80,27 @@ TEST(csrmv_adaptive_double_test, func_check)
     gCsrMat.offRowOff = 0;
 
     double *values = (double*)calloc(num_nonzero, sizeof(double));
-    int *rowIndices = (int*)calloc(num_row+1, sizeof(int));
+    int *rowOffsets = (int*)calloc(num_row+1, sizeof(int));
     int *colIndices = (int*)calloc(num_nonzero, sizeof(int));
     ulong *rowBlocks = (ulong*)calloc(num_nonzero, sizeof(ulong));
 
-    array_view<double> av_values(num_nonzero, values);
-    array_view<int> av_rowOff(num_row+1, rowIndices);
-    array_view<int> av_colIndices(num_nonzero, colIndices);
-    array_view<ulong> av_rowBlocks(num_nonzero, rowBlocks);
 
-    gCsrMat.values = &av_values;
-    gCsrMat.rowOffsets = &av_rowOff;
-    gCsrMat.colIndices = &av_colIndices;
-    gCsrMat.rowBlocks = &av_rowBlocks;
+    gCsrMat.values = am_alloc(sizeof(double) * num_nonzero, acc[1], 0);
+    gCsrMat.rowOffsets = am_alloc(sizeof(int) * (num_row+1), acc[1], 0);
+    gCsrMat.colIndices = am_alloc(sizeof(int) * num_nonzero, acc[1], 0);
+    gCsrMat.rowBlocks = am_alloc(sizeof(ulong) * num_nonzero, acc[1], 0);
 
     status = hcsparseDCsrMatrixfromFile(&gCsrMat, filename, &control, false);
-   
     if (status != hcsparseSuccess)
     {
         std::cout<<"The input file should be in mtx format"<<std::endl;
         exit(1);
     }
+  
+    control.accl_view.copy(gCsrMat.values, values, sizeof(double) * num_nonzero);
+    control.accl_view.copy(gCsrMat.rowOffsets, rowOffsets, sizeof(int) * (num_row+1));
+    control.accl_view.copy(gCsrMat.colIndices, colIndices, sizeof(int) * num_nonzero);
+    control.accl_view.copy(gCsrMat.rowBlocks, rowBlocks, sizeof(ulong) * num_nonzero);
 
     hcsparseCsrMetaSize(&gCsrMat, &control);
 
@@ -107,38 +108,25 @@ TEST(csrmv_adaptive_double_test, func_check)
  
     hcsparseDcsrmv(&gAlpha, &gCsrMat, &gX, &gBeta, &gY, &control); 
 
-    array_view<double> *av_val = static_cast<array_view<double> *>(gCsrMat.values);
-    array_view<int> *av_row = static_cast<array_view<int> *>(gCsrMat.rowOffsets);
-    array_view<int> *av_col = static_cast<array_view<int> *>(gCsrMat.colIndices);
-
     int col = 0;
     for (int row = 0; row < num_row; row++)
     {
         host_res[row] *= host_beta[0];
-        for (; col < (*av_row)[row+1]; col++)
+        for (; col < rowOffsets[row+1]; col++)
         {
-            host_res[row] = host_alpha[0] * host_X[(*av_col)[col]] * (*av_val)[col] + host_res[row];
+            host_res[row] = host_alpha[0] * host_X[colIndices[col]] * values[col] + host_res[row];
         }
     }
 
-    array_view<double> *av_res = static_cast<array_view<double> *>(gY.values);
+    control.accl_view.copy(gY.values, host_Y, sizeof(double) * num_row);
 
     bool isPassed = 1;  
  
     for (int i = 0; i < num_row; i++)
     {
-        double diff = std::abs(host_res[i] - (*av_res)[i]);
+        double diff = std::abs(host_res[i] - host_Y[i]);
         EXPECT_LT(diff, 0.01);
     }
-
-    dev_X.synchronize();
-    dev_Y.synchronize();
-    dev_alpha.synchronize();
-    dev_beta.synchronize();
-    av_values.synchronize();
-    av_rowOff.synchronize();
-    av_colIndices.synchronize();
-    av_rowBlocks.synchronize();
 
     hcsparseTeardown();
 
@@ -148,7 +136,15 @@ TEST(csrmv_adaptive_double_test, func_check)
     free(host_alpha);
     free(host_beta);
     free(values);
-    free(rowIndices);
+    free(rowOffsets);
     free(colIndices);
     free(rowBlocks);
+    am_free(gX.values);
+    am_free(gY.values);
+    am_free(gAlpha.value);
+    am_free(gBeta.value);
+    am_free(gCsrMat.values);
+    am_free(gCsrMat.rowOffsets);
+    am_free(gCsrMat.colIndices);
+    am_free(gCsrMat.rowBlocks);
 }
