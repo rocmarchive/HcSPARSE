e062a8f1a69fefab12f9f6fb84ee912006ccbfc1
Sujitha <sujitha@multicorewareinc.com>
2017-01-03 13:23:04 +0530

    Change array_view to hc_am functions for csrmm & csr_dense routines

--- test/gtest/src/csr_dense_conv_double_test.cpp
+++ test/gtest/src/csr_dense_conv_double_test.cpp
@@ -1,5 +1,6 @@
 #include <hcsparse.h>
 #include <iostream>
+#include <hc_am.hpp>
 #include "gtest/gtest.h"
 
 #define TOLERANCE 0.01
@@ -30,7 +31,7 @@ TEST(csr_dense_conv_double_test, func_check)
 
     gMat.offValues = 0;
 
-    const char* filename = "../../../src/input.mtx";
+    const char* filename = "./../../../../test/gtest/src/input.mtx";
 
     int num_nonzero, num_row, num_col;
 
@@ -47,31 +48,19 @@ TEST(csr_dense_conv_double_test, func_check)
     int *csr_rowOff = (int*)calloc(num_row+1, sizeof(int));
     int *csr_colIndices = (int*)calloc(num_nonzero, sizeof(int));
 
-    array_view<double> av_csr_values(num_nonzero, csr_values);
-    array_view<int> av_csr_rowOff(num_row+1, csr_rowOff);
-    array_view<int> av_csr_colIndices(num_nonzero, csr_colIndices);
-
-    gCsrMat.values = &av_csr_values;
-    gCsrMat.rowOffsets = &av_csr_rowOff;
-    gCsrMat.colIndices = &av_csr_colIndices;
+    gCsrMat.values = (double*) am_alloc(num_nonzero * sizeof(double), acc[1], 0);
+    gCsrMat.rowOffsets = (int*) am_alloc((num_row+1) * sizeof(int), acc[1], 0);
+    gCsrMat.colIndices = (int*) am_alloc(num_nonzero * sizeof(int), acc[1], 0);
 
     double *csr_res_values = (double*)calloc(num_nonzero, sizeof(double));
     int *csr_res_rowOff = (int*)calloc(num_row+1, sizeof(int));
     int *csr_res_colIndices = (int*)calloc(num_nonzero, sizeof(int));
 
-    array_view<double> av_csr_res_values(num_nonzero, csr_res_values);
-    array_view<int> av_csr_res_rowOff(num_row+1, csr_res_rowOff);
-    array_view<int> av_csr_res_colIndices(num_nonzero, csr_res_colIndices);
+    gCsrMat_res.values = (double*) am_alloc(num_nonzero * sizeof(double), acc[1], 0);
+    gCsrMat_res.rowOffsets = (int*) am_alloc((num_row+1) * sizeof(int), acc[1], 0);
+    gCsrMat_res.colIndices = (int*) am_alloc(num_nonzero * sizeof(int), acc[1], 0);
 
-    gCsrMat_res.values = &av_csr_res_values;
-    gCsrMat_res.rowOffsets = &av_csr_res_rowOff;
-    gCsrMat_res.colIndices = &av_csr_res_colIndices;
-
-    double *A_values = (double*)calloc(num_row*num_col, sizeof(double));
-
-    array_view<double> av_A_values(num_row*num_col, A_values);
-
-    gMat.values = &av_A_values;
+    gMat.values = (double*) am_alloc(num_row*num_col * sizeof(double), acc[1], 0);
     gMat.num_rows = num_row;
     gMat.num_cols = num_col;
 
@@ -79,33 +68,34 @@ TEST(csr_dense_conv_double_test, func_check)
 
     hcsparseDcsr2dense(&gCsrMat, &gMat, &control);
 
+    control.accl_view.copy(gCsrMat.values, csr_values, num_nonzero * sizeof(double));
+    control.accl_view.copy(gCsrMat.rowOffsets, csr_rowOff, (num_row+1) * sizeof(int));
+    control.accl_view.copy(gCsrMat.colIndices, csr_colIndices, num_nonzero * sizeof(int));
+
     hcsparseDdense2csr(&gMat, &gCsrMat_res, &control);
+
+    control.accl_view.copy(gCsrMat_res.values, csr_res_values, num_nonzero * sizeof(double));
+    control.accl_view.copy(gCsrMat_res.rowOffsets, csr_res_rowOff, (num_row+1) * sizeof(int));
+    control.accl_view.copy(gCsrMat_res.colIndices, csr_res_colIndices, num_nonzero * sizeof(int));
 
     bool ispassed = 1;
 
     for (int i = 0; i < num_nonzero; i++)
     {
-        double diff = std::abs(av_csr_values[i] - av_csr_res_values[i]);
+        double diff = std::abs(csr_values[i] - csr_res_values[i]);
         EXPECT_LT(diff, TOLERANCE);
     }
 
     for (int i = 0; i < num_nonzero; i++)
     {
-        EXPECT_EQ(av_csr_colIndices[i], av_csr_res_colIndices[i]);
+        EXPECT_EQ(csr_colIndices[i], csr_res_colIndices[i]);
     }
 
     for (int i = 0; i < num_row+1; i++)
     {
-        EXPECT_EQ(av_csr_rowOff[i], av_csr_res_rowOff[i]);
+        EXPECT_EQ(csr_rowOff[i], csr_res_rowOff[i]);
     }
 
-    av_csr_values.synchronize();
-    av_csr_rowOff.synchronize();
-    av_csr_colIndices.synchronize();
-    av_csr_res_values.synchronize();
-    av_csr_res_rowOff.synchronize();
-    av_csr_res_colIndices.synchronize();
-    av_A_values.synchronize();
 
     hcsparseTeardown();
 
@@ -115,6 +105,12 @@ TEST(csr_dense_conv_double_test, func_check)
     free(csr_res_values);
     free(csr_res_rowOff);
     free(csr_res_colIndices);
-    free(A_values);
+    am_free(gCsrMat.values);
+    am_free(gCsrMat.rowOffsets);
+    am_free(gCsrMat.colIndices);
+    am_free(gCsrMat_res.values);
+    am_free(gCsrMat_res.rowOffsets);
+    am_free(gCsrMat_res.colIndices);
+    am_free(gMat.values);
 }
 
