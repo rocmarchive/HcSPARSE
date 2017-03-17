d83131b717359c35195d5008c55a5b5bf0c246bf
Santhosh Kumar Rajamanickam <santhosh@multicorewareinc.com>
2017-03-17 13:52:04 +0000

    Add logic for few supported hipSPARSE APIs
    - Add logic to support the following hipSPARSE APIs
        hipsparseScsrmv
        hipsparseDcsrmv
        hipsparseScsrmm
        hipsparseDcsrmm
        hipsparseScsrgemm
        hipsparseSdense2csr
        hipsparseDdense2csr
        hipsparseScsr2dense
        hipsparseDcsr2dense
        hipsparseXcoo2csr
        hipsparseXcsr2coo
    - Still more APIs to support.
    
    hcsparse: hipify-sparse

---
+++ lib/include/hipsparse.h
@@ -0,0 +1,57 @@
+/*
+Copyright (c) 2015-2016 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+//! HIP = Heterogeneous-compute Interface for Portability
+//!
+//! Define a extremely thin runtime layer that allows source code to be compiled unmodified 
+//! through either AMD HCC or NVCC.   Key features tend to be in the spirit
+//! and terminology of CUDA, but with a portable path to other accelerators as well.
+//!
+//!  This is the master include file for hipsparse, wrapping around hcsparse and cusparse "version 1"
+//
+
+#pragma once
+
+enum hipsparseStatus_t {
+  HIPSPARSE_STATUS_SUCCESS,          // Function succeeds
+  HIPSPARSE_STATUS_NOT_INITIALIZED,  // HIPSPARSE library not initialized
+  HIPSPARSE_STATUS_ALLOC_FAILED,     // resource allocation failed
+  HIPSPARSE_STATUS_INVALID_VALUE,    // unsupported numerical value was passed to function
+  HIPSPARSE_STATUS_MAPPING_ERROR,    // access to GPU memory space failed
+  HIPSPARSE_STATUS_EXECUTION_FAILED, // GPU program failed to execute
+  HIPSPARSE_STATUS_INTERNAL_ERROR,    // an internal HIPSPARSE operation failed
+  HIPSPARSE_STATUS_NOT_SUPPORTED     // cuSPARSE supports this, but not hcSPARSE
+};
+
+// Some standard header files, these are included by hc.hpp and so want to make them avail on both
+// paths to provide a consistent include env and avoid "missing symbol" errors that only appears
+// on NVCC path:
+
+#if defined(__HIP_PLATFORM_HCC__) and not defined (__HIP_PLATFORM_NVCC__)
+#include <hcc_detail/hip_sparse.h>
+#elif defined(__HIP_PLATFORM_NVCC__) and not defined (__HIP_PLATFORM_HCC__)
+#include <nvcc_detail/hip_sparse.h>
+#else 
+#error("Must define exactly one of __HIP_PLATFORM_HCC__ or __HIP_PLATFORM_NVCC__");
+#endif 
+
+
---
+++ lib/include/nvcc_detail/hip_sparse.h
@@ -0,0 +1,138 @@
+/*
+Copyright (c) 2015-2016 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#pragma once
+
+#include <cuda_runtime_api.h>
+#include <cusparse.h>
+
+//HGSOS for Kalmar leave it as C++, only cuSPARSE needs C linkage.
+
+#ifdef cplusplus
+extern "C" {
+#endif
+
+typedef cusparseHandle_t hipsparseHandle_t ;
+typedef cusparseMatDescr_t hipsparseMatDescr_t ;
+typedef cusparseIndexBase_t hipsparseIndexBase_t;
+typedef cusparseOperation_t hipsparseOperation_t;
+
+hipsparseStatus_t hipCUSPARSEStatusToHIPStatus(cusparseStatus_t cuStatus); 
+
+//Sparse L2 BLAS operations
+
+hipsparseStatus_t hipsparseScsrmv(hipsparseHandle_t handle, hipsparseOperation_t transA, 
+                                  int m, int n, int nnz, const float           *alpha, 
+                                  const hipsparseMatDescr_t descrA, 
+                                  const float           *csrValA, 
+                                  const int *csrRowPtrA, const int *csrColIndA,
+                                  const float           *x, const float           *beta, 
+                                  float           *y);
+
+hipsparseStatus_t hipsparseDcsrmv(hipsparseHandle_t handle, hipsparseOperation_t transA, 
+                                  int m, int n, int nnz, const double           *alpha, 
+                                  const hipsparseMatDescr_t descrA, 
+                                  const double           *csrValA, 
+                                  const int *csrRowPtrA, const int *csrColIndA,
+                                  const double           *x, const double           *beta, 
+                                  double           *y);
+
+//Sparse L3 BLAS operations
+
+hipsparseStatus_t hipsparseScsrmm(hipsparseHandle_t handle, 
+                                                hipsparseOperation_t transA, 
+                                                int m, int n, int k, int nnz, 
+                                                const float           *alpha, 
+                                                const hipsparseMatDescr_t descrA, 
+                                                const float             *csrValA, 
+                                                const int             *csrRowPtrA, 
+                                                const int             *csrColIndA,
+                                                const float *B,             int ldb,
+                                                const float *beta, float *C, int ldc);
+
+hipsparseStatus_t hipsparseDcsrmm(hipsparseHandle_t handle, 
+                                                hipsparseOperation_t transA, 
+                                                int m, int n, int k, int nnz, 
+                                                const double           *alpha, 
+                                                const hipsparseMatDescr_t descrA, 
+                                                const double             *csrValA, 
+                                                const int             *csrRowPtrA, 
+                                                const int             *csrColIndA,
+                                                const double *B,             int ldb,
+                                                const double *beta, double *C, int ldc);
+
+hipsparseStatus_t hipsparseScsrgemm(hipsparseHandle_t handle,
+                                                  hipsparseOperation_t transA, 
+                                                  hipsparseOperation_t transB,
+                                                  int m, int n, int k,
+                                                  const hipsparseMatDescr_t descrA, 
+                                                  const int nnzA, const float *csrValA,
+                                                  const int *csrRowPtrA, 
+                                                  const int *csrColIndA,
+                                                  const hipsparseMatDescr_t descrB, 
+                                                  const int nnzB, const float *csrValB, 
+                                                  const int *csrRowPtrB, 
+                                                  const int *csrColIndB,
+                                                  const hipsparseMatDescr_t descrC,
+                                                  float *csrValC, const int *csrRowPtrC, 
+                                                  int *csrColIndC );
+
+//Matrix conversion routines
+
+hipsparseStatus_t hipsparseSdense2csr(hipsparseHandle_t handle, int m, int n, 
+                                                    const hipsparseMatDescr_t descrA, 
+                                                    const float           *A, 
+                                                    int lda, const int *nnzPerRow, 
+                                                    float           *csrValA, 
+                                                    int *csrRowPtrA, int *csrColIndA);
+
+hipsparseStatus_t hipsparseDdense2csr(hipsparseHandle_t handle, int m, int n, 
+                                                    const hipsparseMatDescr_t descrA, 
+                                                    const double           *A, 
+                                                    int lda, const int *nnzPerRow, 
+                                                    double           *csrValA, 
+                                                    int *csrRowPtrA, int *csrColIndA);
+
+hipsparseStatus_t hipsparseScsr2dense(hipsparseHandle_t handle, int m, int n, 
+                                                    const hipsparseMatDescr_t descrA, 
+                                                    const float             *csrValA, 
+                                                    const int *csrRowPtrA, const int *csrColIndA,
+                                                    float *A, int lda);
+
+hipsparseStatus_t hipsparseDcsr2dense(hipsparseHandle_t handle, int m, int n, 
+                                                    const hipsparseMatDescr_t descrA, 
+                                                    const double             *csrValA, 
+                                                    const int *csrRowPtrA, const int *csrColIndA,
+                                                    double *A, int lda);
+
+hipsparseStatus_t hipsparseXcoo2csr(hipsparseHandle_t handle, const int *cooRowIndA, 
+                                                  int nnz, int m, int *csrRowPtrA, 
+                                                  hipsparseIndexBase_t idxBase);
+
+hipsparseStatus_t hipsparseXcsr2coo(hipsparseHandle_t handle, const int *csrRowPtrA,
+                                                  int nnz, int m, int *cooRowIndA, 
+                                                  hipsparseIndexBase_t idxBase);
+
+#ifdef __cplusplus
+}
+#endif
+
+
