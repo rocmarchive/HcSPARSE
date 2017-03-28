4211d8b7871a9c6f469e19bb5a7fd67d95354296
Sujitha <sujitha@multicorewareinc.com>
2017-03-28 17:07:53 +0530

    Test files for the API added recently - yet to test it

---
+++ test/unit-API/src/csc_dense_double_test_API.cpp
@@ -0,0 +1,195 @@
+#include <hcsparse.h>
+#include <iostream>
+#include "hc_am.hpp"
+
+int main(int argc, char *argv[])
+{
+#if 0
+    std::vector<accelerator>acc = accelerator::get_all();
+    accelerator_view accl_view = (acc[1].create_view()); 
+
+    hcsparseControl control(accl_view);
+    hcsparseSetup();
+
+    if (argc != 2)
+    {
+        std::cout<<"Required mtx input file"<<std::endl;
+        return 0;
+    }
+
+    const char* filename = argv[1];
+    int num_nonzero, num_row, num_col;
+    hcsparseStatus status;
+
+   status = hcsparseHeaderfromFile(&num_nonzero, &num_row, &num_col, filename);
+    if (status != hcsparseSuccess)
+    {
+        std::cout<<"The input file should be in mtx format"<<std::endl;
+        return 0;
+    }
+
+    hcsparseSCsrMatrixfromFile(&gCsrMat, filename, &control, false);
+#else
+
+    int num_row = 4;
+    int num_col = 5;
+    int num_nonzero = 9;
+
+    double* valPtr = (double*)calloc(num_row*num_col, sizeof(double));
+    int* colPtr = (int*)calloc(num_col+1, sizeof(int));
+    int* rowInd = (int*)calloc(num_nonzero, sizeof(int));
+
+    valPtr[0] = 1;
+    valPtr[1] = 5;
+    valPtr[2] = 4;
+    valPtr[3] = 2;
+    valPtr[4] = 3;
+    valPtr[5] = 9;
+    valPtr[6] = 7;
+    valPtr[7] = 8;
+    valPtr[8] = 6;
+
+    colPtr[0] = 0;
+    colPtr[1] = 2;
+    colPtr[2] = 4;
+    colPtr[3] = 6;
+    colPtr[4] = 7;
+    colPtr[5] = 9;
+
+    rowInd[0] = 0;
+    rowInd[1] = 2;
+    rowInd[2] = 0;
+    rowInd[3] = 1;
+    rowInd[4] = 1;
+    rowInd[5] = 3;
+    rowInd[6] = 2;
+    rowInd[7] = 2;
+    rowInd[8] = 3;
+#endif
+
+     /* Test New APIs */
+    hcsparseHandle_t handle;
+    hcsparseStatus_t status1;
+    hc::accelerator accl;
+    hc::accelerator_view av = accl.get_default_view();
+
+    status1 = hcsparseCreate(&handle, &av);
+    if (status1 != HCSPARSE_STATUS_SUCCESS) {
+      std::cout << "Error Initializing the sparse library."<<std::endl;
+      return -1;
+    }
+    std::cout << "Successfully initialized sparse library"<<std::endl;
+
+    hcsparseMatDescr_t descrA;
+
+    status1 = hcsparseCreateMatDescr(&descrA);
+    if (status1 != HCSPARSE_STATUS_SUCCESS) {
+      std::cout << "error creating mat descrptr"<<std::endl;
+      return -1;
+    }
+    std::cout << "successfully created mat descriptor"<<std::endl;
+
+    double* cscValA = (double*) am_alloc(num_nonzero * sizeof(double), handle->currentAccl, 0);
+    int* cscColPtrA = (int*) am_alloc((num_col+1) * sizeof(int), handle->currentAccl, 0);
+    int* cscRowIndA = (int*) am_alloc(num_nonzero * sizeof(int), handle->currentAccl, 0);
+    double* A = (double*) am_alloc(num_row*num_col * sizeof(double), handle->currentAccl, 0);
+
+    double *csc_val = (double*)calloc(num_nonzero, sizeof(double));
+    int *csc_colPtr = (int*)calloc(num_col+1, sizeof(int));
+    int *csc_rowInd = (int*)calloc(num_nonzero, sizeof(int));    
+
+    double *csc_res_val = (double*)calloc(num_nonzero, sizeof(double));
+    int *csc_res_colPtr = (int*)calloc(num_col+1, sizeof(int));
+    int *csc_res_rowInd = (int*)calloc(num_nonzero, sizeof(int));    
+     
+    handle->currentAcclView.copy(valPtr, cscValA, num_nonzero * sizeof(double));
+    handle->currentAcclView.copy(colPtr, cscColPtrA, (num_col+1) * sizeof(int));
+    handle->currentAcclView.copy(rowInd, cscRowIndA, num_nonzero * sizeof(int));
+
+    status1 = hcsparseDcsc2dense(handle, num_row, num_col,
+                                 descrA, cscValA, cscColPtrA, cscRowIndA, A, num_col);
+    if (status1 != HCSPARSE_STATUS_SUCCESS) {
+      std::cout << "Error csc2dense conversion "<<std::endl;
+      return -1;
+    }
+    std::cout << "csc2dense conv. - success"<<std::endl;
+
+    handle->currentAcclView.copy(cscValA, csc_val, num_nonzero * sizeof(double));
+    handle->currentAcclView.copy(cscColPtrA, csc_colPtr, (num_col+1) * sizeof(int));
+    handle->currentAcclView.copy(cscRowIndA, csc_rowInd, num_nonzero * sizeof(int));
+
+#if DEBUG
+    double* A_h = (double*) calloc(num_row*num_col, sizeof(double));
+    handle->currentAcclView.copy(A, A_h, num_row*num_col * sizeof(double));
+    std::cout << "After conversion - csrval" << std::endl;
+    for (int i = 0; i < num_nonzero; i++)
+      std::cout << i << " " << csc_val[i] <<  std::endl;
+
+    std::cout << "After conversion - dense A " << std::endl;
+    for (int i = 0; i < num_row*num_col; i++)
+      std::cout << i << " " << A_h[i] <<  std::endl;
+#endif
+
+
+    int nnzperrow = 0;
+    status1 = hcsparseDdense2csc(handle, num_row, num_col,
+                                 descrA, A, num_row, &nnzperrow, cscValA, cscColPtrA, cscRowIndA);
+    if (status1 != HCSPARSE_STATUS_SUCCESS) {
+      std::cout << "Error dense2csc conversion "<<std::endl;
+      return -1;
+    }
+    std::cout << "dense2csc conv. - success"<<std::endl;
+
+    handle->currentAcclView.copy(cscValA, csc_res_val, num_nonzero * sizeof(double));
+    handle->currentAcclView.copy(cscColPtrA, csc_res_colPtr, (num_col+1) * sizeof(int));
+    handle->currentAcclView.copy(cscRowIndA, csc_res_rowInd, num_nonzero * sizeof(int));
+
+    bool ispassed = 1;
+
+    std::cout <<"Val check" << std::endl;
+    for (int i = 0; i < num_nonzero; i++)
+    {
+        double diff = std::abs(csc_val[i] - csc_res_val[i]);
+        if (diff > 0.01)
+        {
+            std::cout << i << " " << csc_val[i] << " " << csc_res_val[i] << std::endl;
+            ispassed = 0;
+            break;
+        }
+    }
+
+    std::cout <<"rowind check" << std::endl;
+    for (int i = 0; i < num_nonzero; i++)
+    {
+        if (csc_rowInd[i] != csc_res_rowInd[i])
+        {
+            std::cout << i << " " << csc_rowInd[i] << " " << csc_res_rowInd[i] << std::endl;
+            ispassed = 0;
+            break;
+        }
+    }
+
+    std::cout <<"col check" << std::endl;
+    for (int i = 0; i < num_col+1; i++)
+    {
+        if (csc_colPtr[i] != csc_res_colPtr[i])
+        {
+            std::cout << i << " " << csc_colPtr[i] << " " << csc_res_colPtr[i] << std::endl;
+            ispassed = 0;
+            break;
+        }
+    }
+    std::cout << (ispassed?"TEST PASSED":"TEST FAILED") << std::endl;
+
+    free(csc_val);
+    free(csc_colPtr);
+    free(csc_rowInd);
+    free(csc_res_val);
+    free(csc_res_colPtr);
+    free(csc_res_rowInd);
+    am_free(cscValA);
+    am_free(cscColPtrA);
+    am_free(cscRowIndA);
+
+    return 0;
+}
---
+++ test/unit-API/src/csrmm_double_test_API.cpp
@@ -0,0 +1,193 @@
+#include <hcsparse.h>
+#include <iostream>
+#include "hc_am.hpp"
+int main(int argc, char *argv[])
+{
+
+    std::vector<accelerator>acc = accelerator::get_all();
+    accelerator_view accl_view = (acc[1].create_view()); 
+
+    hcsparseControl control(accl_view);
+
+    if (argc != 2)
+    {
+        std::cout<<"Required mtx input file"<<std::endl;
+        return 0;
+    }
+
+    const char* filename = argv[1];
+
+    int num_nonzero, num_row_A, num_col_A;
+
+    hcsparseStatus status;
+
+    status = hcsparseHeaderfromFile(&num_nonzero, &num_row_A, &num_col_A, filename);
+    if (status != hcsparseSuccess)
+    {
+        std::cout<<"The input file should be in mtx format"<<std::endl;
+        return 0;
+    } 
+
+     /* Test New APIs */
+    hcsparseHandle_t handle;
+    hcsparseStatus_t status1;
+    hc::accelerator accl;
+    hc::accelerator_view av = accl.get_default_view();
+
+    status1 = hcsparseCreate(&handle, &av);
+    if (status1 != HCSPARSE_STATUS_SUCCESS) {
+      std::cout << "Error Initializing the sparse library."<<std::endl;
+      return -1;
+    }
+    std::cout << "Successfully initialized sparse library"<<std::endl;
+
+    hcsparseMatDescr_t descrA;
+
+    status1 = hcsparseCreateMatDescr(&descrA);
+    if (status1 != HCSPARSE_STATUS_SUCCESS) {
+      std::cout << "error creating mat descrptr"<<std::endl;
+      return -1;
+    }
+    std::cout << "successfully created mat descriptor"<<std::endl;
+
+    int num_row_X, num_col_X, num_row_Y, num_col_Y;
+
+    num_row_X = num_col_A;
+    num_col_X = rand()%100;
+    num_row_Y = num_row_A;
+    num_col_Y = num_col_X;
+
+    double *host_res = (double*) calloc(num_row_Y * num_col_Y, sizeof(double));
+    double *host_X = (double*) calloc(num_row_X * num_col_X, sizeof(double));
+    double *host_Y = (double*) calloc(num_row_Y * num_col_Y, sizeof(double));
+    double *host_alpha = (double*) calloc(1, sizeof(double));
+    double *host_beta = (double*) calloc(1, sizeof(double));
+
+    hcsparseCsrMatrix gCsrMat;
+    double *gX;
+    double *gY;
+    double *gAlpha;
+    double *gBeta;
+
+    hcsparseSetup();
+    hcsparseInitCsrMatrix(&gCsrMat);
+
+    gX = am_alloc(sizeof(double) * num_col_X * num_row_X, acc[1], 0);
+    gY = am_alloc(sizeof(double) * num_row_Y * num_col_Y, acc[1], 0);
+    gAlpha = am_alloc(sizeof(double) * 1, acc[1], 0);
+    gBeta = am_alloc(sizeof(double) * 1, acc[1], 0);
+
+    srand (time(NULL));
+    for (int i = 0; i < num_col_X * num_row_X; i++)
+    {
+       host_X[i] = rand()%100;
+    }
+
+    for (int i = 0; i < num_row_Y * num_col_Y; i++)
+    {
+        host_res[i] = host_Y[i] = rand()%100;
+    }
+
+    host_alpha[0] = rand()%100;
+    host_beta[0] = rand()%100;
+
+    control.accl_view.copy(host_X, gX, sizeof(double) * num_col_X * num_row_X);
+    control.accl_view.copy(host_Y, gY, sizeof(double) * num_row_Y * num_col_Y);
+    control.accl_view.copy(host_alpha, gAlpha, sizeof(double) * 1);
+    control.accl_view.copy(host_beta, gBeta, sizeof(double) * 1);
+
+    double *values = (double*)calloc(num_nonzero, sizeof(double));
+    int *rowOffsets = (int*)calloc(num_row_A+1, sizeof(int));
+    int *colIndices = (int*)calloc(num_nonzero, sizeof(int));
+
+    gCsrMat.values = am_alloc(sizeof(double) * num_nonzero, acc[1], 0);
+    gCsrMat.rowOffsets = am_alloc(sizeof(int) * (num_row_A+1), acc[1], 0);
+    gCsrMat.colIndices = am_alloc(sizeof(int) * num_nonzero, acc[1], 0);
+
+    status = hcsparseSCsrMatrixfromFile(&gCsrMat, filename, &control, false);
+    if (status != hcsparseSuccess)
+    {
+        std::cout<<"The input file should be in mtx format"<<std::endl;
+        return 0;
+    }
+
+    control.accl_view.copy(gCsrMat.values, values, sizeof(double) * num_nonzero);
+    control.accl_view.copy(gCsrMat.rowOffsets, rowOffsets, sizeof(int) * (num_row_A+1));
+    control.accl_view.copy(gCsrMat.colIndices, colIndices, sizeof(int) * num_nonzero);
+
+    hcsparseOperation_t transA = HCSPARSE_OPERATION_NON_TRANSPOSE;
+    int nnz = 0;
+
+    status1 = hcsparseScsrmm(handle, transA, num_row_A, num_col_Y,
+                            num_col_A, nnz, static_cast<const double*>(gAlpha), descrA,
+                            static_cast<const double*>(gCsrMat.values),
+                            (int *) gCsrMat.rowOffsets,
+                            (int *)gCsrMat.colIndices, (double*)gX, num_col_X,
+                            static_cast<const double*>(gBeta), (double *)gY, num_col_Y);
+
+    for (int col = 0; col < num_col_X; col++)
+    {
+        int indx = 0;
+        for (int row = 0; row < num_row_A; row++)
+        {
+            double sum = 0.0;
+            for (; indx < rowOffsets[row+1]; indx++)
+            {
+                sum += host_alpha[0] * host_X[colIndices[indx] * num_col_X + col] * values[indx];
+            }
+            host_res[row * num_col_Y + col] = sum + host_beta[0] * host_res[row * num_col_Y + col];
+        }
+    }
+
+    control.accl_view.copy(gY, host_Y, sizeof(double) * num_row_Y * num_col_Y);
+
+    bool isPassed = 1;
+
+    for (int i = 0; i < num_row_Y * num_col_Y; i++)
+    {
+        double diff = std::abs(host_res[i] - host_Y[i]);
+        if (diff > 0.01)
+        {
+            std::cout<<i<<" "<<host_res[i]<<" "<<host_Y[i]<< " "<<diff<<std::endl;
+            isPassed = 0;
+            break;
+        }
+    }
+
+    std::cout << (isPassed?"TEST PASSED":"TEST FAILED") << std::endl;
+
+    status1 = hcsparseDestroyMatDescr(&descrA);
+    if (status1 != HCSPARSE_STATUS_SUCCESS) {
+      std::cout << "error creating mat descrptr"<<std::endl;
+      return -1;
+    }
+    std::cout << "successfully created mat descriptor"<<std::endl;
+
+    status1 = hcsparseDestroy(&handle);
+    if (status1 != HCSPARSE_STATUS_SUCCESS) {
+      std::cout << "Error DeInitializing the sparse library."<<std::endl;
+      return -1;
+    }
+    std::cout << "Successfully deinitialized sparse library"<<std::endl;
+   
+    /* End - Test of New APIs */
+
+    free(host_res);
+    free(host_X);
+    free(host_Y);
+    free(host_alpha);
+    free(host_beta);
+    free(values);
+    free(rowOffsets);
+    free(colIndices);
+    am_free(gX);
+    am_free(gY);
+    am_free(gAlpha);
+    am_free(gBeta);
+    am_free(gCsrMat.values);
+    am_free(gCsrMat.rowOffsets);
+    am_free(gCsrMat.colIndices);
+
+
+    return 0;
+}
