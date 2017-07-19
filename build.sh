36371a25544fdf050750b461cc78e90528c1f59f
Neelakandan <neelakandan@multicorewareinc.com>
2017-07-19 17:36:41 +0530

    Add install, debug options and multithreading support to build script

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -e
 #This script is invoked to install the hcsparse library and test sources
 
-# CURRENT_WORK_DIRECTORY
-current_work_dir=$PWD
+# getconf _NPROCESSORS_ONLN
+working_threads=8
 
 # CHECK FOR COMPILER PATH
-if [ ! -z $MCWHCCBUILD ]
+if [ ! -z $HCC_HOME ]
 then
   platform="hcc"
-  if [ -x "$MCWHCCBUILD/bin/clang++" ]
+  if [ -x "$HCC_HOME/bin/clang++" ]
   then
-    cmake_c_compiler="$MCWHCCBUILD/bin/clang"
-    cmake_cxx_compiler="$MCWHCCBUILD/bin/clang++"
+    cmake_c_compiler="$HCC_HOME/bin/clang"
+    cmake_cxx_compiler="$HCC_HOME/bin/clang++"
   fi
 elif [ -x "/opt/rocm/hcc/bin/clang++" ]
 then
@@ -36,7 +36,8 @@ elif ( [ "$platform" = "nvcc" ]); then
   exit 1
 fi
 
-export CLAMP_NOTILECHECK=ON
+#CURRENT_WORK_DIRECTORY
+current_work_dir=$PWD
 
 if [ ! -z $HIP_PATH ]
 then
@@ -48,15 +49,22 @@ fi
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
+copt="-O3"
+verbose=""
+install=0
 
 # Help menu
 print_help() {
 cat <<-HELP
 =============================================================================================================================
-This script is invoked to install hcblas library and test sources. Please provide the following arguments:
+This script is invoked to build and install hcSPARSE library and test sources. Please provide the following arguments:
 
-  1) ${green}--test${reset}    Test to enable the library testing. 
-
+  ${green}--test${reset}    Test to enable the library testing. 
+  ${green}--debug${reset}    Compile with debug info (-g)
+  ${green}--verbose${reset}  Run make with VERBOSE=1
+  ${green}--install${reset}  Install the shared library and include the header files under /opt/rocm/hcblas  Requires sudo perms.
+  ${green}--examples${reset} To build and run the example files in examples folder (on/off) (ONLY SUPPORTED ON AMD PLATFORM)
+  
 =============================================================================================================================
 HELP
 exit 0
@@ -66,6 +74,18 @@ while [ $# -gt 0 ]; do
   case "$1" in
     --test=*)
       testing="${1#*=}"
+      ;;
+    --debug)
+      copt="-g"
+      ;;
+    --verbose)
+      verbose="VERBOSE=1"
+      ;;
+    --install)
+      install="1"
+      ;;
+    --examples=*)
+      examples="${1#*=}"
       ;;
     --install)
       install="1"
@@ -86,8 +106,8 @@ fi
 
 set +e
 # MAKE BUILD DIR
-mkdir $current_work_dir/build
-mkdir $current_work_dir/build/test
+mkdir -p $current_work_dir/build
+mkdir -p $current_work_dir/build/packaging
 set -e
 
 # SET BUILD DIR
@@ -97,34 +117,35 @@ build_dir=$current_work_dir/build
 cd $build_dir
 
 if [ "$platform" = "hcc" ]; then
-  
-   export HCSPARSE_LIBRARY_PATH=$current_work_dir/build/lib/src
-   export LD_LIBRARY_PATH=$HCSPARSE_LIBRARY_PATH:$LD_LIBRARY_PATH
+    #default case: Of course there are Compulsory waits on certain kernels 
+    cmake -DCMAKE_C_COMPILER=$cmake_c_compiler  -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS="$copt -fPIC" -DCMAKE_INSTALL_PREFIX=/opt/rocm/hcsparse $current_work_dir
 
+  make -j$working_threads package $verbose
+  make -j$working_threads $verbose
 
-# Cmake and make libhcblas: Install hcblas
-   cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=/opt/rocm/hcsparse $current_work_dir
-   make package
-   make
+  export HCSPARSE_LIBRARY_PATH=$current_work_dir/build/lib/src
+  export LD_LIBRARY_PATH=$HCSPARSE_LIBRARY_PATH:$LD_LIBRARY_PATH
 
    if [ "$install" = "1" ]; then
-     sudo make install
-     cd $build_dir/packaging/ && cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=/opt/rocm/hcsparse $current_work_dir/packaging/
+     sudo make -j$working_threads install
    fi
+   cd $build_dir/packaging/ && cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=/opt/rocm/hcsparse $current_work_dir/packaging/
+   
+  echo "${green}hcSPARSE Build Completed!${reset}"
 
-# Test=OFF (Build library and tests)
-  if ( [ -z $testing ] ) || ( [ "$testing" = "off" ] ); then
-    echo "${green}HCSPARSE Installation Completed!${reset}"
 # Test=ON (Build and test the library)
-  elif ( [ "$testing" = "on" ] ); then
+  if ( [ "$testing" = "on" ] ); then
 # Build Tests
      set +e
-     mkdir $current_work_dir/build/test/unit_test/bin/
-     mkdir $current_work_dir/build/test/gtest/bin/
+     mkdir -p $current_work_dir/build/test
+     mkdir -p $current_work_dir/build/test/unit_test/bin/
+     mkdir -p $current_work_dir/build/test/gtest/bin/
+     mkdir -p $current_work_dir/build/test/unit-hip/bin/
+     mkdir -p $current_work_dir/build/test/unit-API/bin/
      set -e
      
      cd $build_dir/test/ && cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC $current_work_dir/test/
-     make
+     make -j$working_threads
 
 # Invoke hc unit test script
      printf "* UNIT API TESTS *\n"
@@ -142,15 +163,32 @@ if [ "$platform" = "hcc" ]; then
   fi
 fi
 
+#EXAMPLES
+#Invoke examples script if --examples=on
+  if [ "$examples" = "on" ]; then
+    printf "* EXAMPLES *\n"
+    printf "************\n"
+    chmod +x $current_work_dir/examples/build.sh
+    cd $current_work_dir/examples/
+    ./build.sh
+  fi
+
 if [ "$platform" = "nvcc" ]; then
   
   export HIPSPARSE_LIBRARY_PATH=$current_work_dir/build/lib/src
   export LD_LIBRARY_PATH=$HIPSPARSE_LIBRARY_PATH:$LD_LIBRARY_PATH
   
   cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=/opt/rocm/hcsparse $current_work_dir
-  make package
-  make
-  echo "${green}HIPSPARSE Build Completed!${reset}"
+
+  make -j$working_threads package $verbose
+  make -j$working_threads $verbose
+
+  if [ "$install" = "1" ]; then
+    sudo -j$working_threads make install
+  fi
+  cd $build_dir/packaging/ && cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_INSTALL_PREFIX=/opt/rocm/hcsparse $current_work_dir/packaging/ 
+
+  echo "${green}hipSPARSE Build Completed!${reset}"
 
   if  [ "$testing" = "on" ]; then
        cd $build_dir/test/ && cmake -DCMAKE_C_COMPILER=$cmake_c_compiler  -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC $current_work_dir/test/
