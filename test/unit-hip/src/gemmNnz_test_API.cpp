7a2c7982ba248f9c993788cbba935fe98ea08352
Yuvaraj Venkatesh <yuvaraj@multicorewareinc.com>
2017-10-25 17:19:37 +0530

    Written test case for gemmNnz routine

---
+++ test/unit-hip/src/gemmNnz_test_API.cpp
@@ -0,0 +1,203 @@
+#include "hip/hip_runtime.h"
+#include "hipsparse.h"
+#include <iostream>
+#include "gtest/gtest.h"
+
+TEST(gemmNnz_test, func_check)
+{
+     /* Test New APIs */
+    hipsparseHandle_t handle;
+    hipsparseStatus_t status1;
+    hipsparseMatDescr_t descrA;
+    hipsparseMatDescr_t descrB;
+    hipsparseMatDescr_t descrC;
+
+    status1 = hipsparseCreate(&handle);
+    if (status1 != HIPSPARSE_STATUS_SUCCESS) {
+      std::cout << "Error Initializing the sparse library."<<std::endl;
+      exit(1);
+    }
+
+    hipsparseDirection_t dir = HIPSPARSE_DIRECTION_COLUMN;
+
+    int m = 64;
+    int n = 64;
+    int k = 64;
+
+    status1 = hipsparseCreateMatDescr(&descrA);
+    status1 = hipsparseCreateMatDescr(&descrB);
+    status1 = hipsparseCreateMatDescr(&descrC);
+    if (status1 != HIPSPARSE_STATUS_SUCCESS) {
+      std::cout << "error creating mat descrptr"<<std::endl;
+      exit(1);
+    }
+
+    float *devA = NULL;
+    float *devB = NULL; 
+    int *nnzPerRowColumnA = NULL;
+    int *nnzA = NULL;
+    int *nnzPerRowColumnB = NULL;
+    int *nnzB = NULL;
+    hipError_t err;
+
+    err = hipMalloc(&devA, sizeof(float)*m*k);
+    err = hipMalloc(&devB, sizeof(float)*k*n);
+    err = hipMalloc(&nnzPerRowColumnA, sizeof(int)*m);
+    err = hipMalloc(&nnzA, sizeof(int) * 1);
+    err = hipMalloc(&nnzPerRowColumnB, sizeof(int)*k);
+    err = hipMalloc(&nnzB, sizeof(int) * 1);
+
+    float *hostA = (float*)calloc (m*k, sizeof(float));
+    float *hostB = (float*)calloc (k*n, sizeof(float));
+    float *hostC = (float*)calloc (m*n, sizeof(float));
+
+    srand (time(NULL));
+    for (int i = 0; i < m*k; i++)
+    {
+        hostA[i] = rand()%2;
+    }    
+
+    srand (time(NULL));
+    for (int i = 0; i < k*n; i++)
+    {
+        hostB[i] = rand()%2;
+    }
+    
+    hipMemcpy(devA, hostA, m*k*sizeof(float), hipMemcpyHostToDevice);
+    hipMemcpy(devB, hostB, k*n*sizeof(float), hipMemcpyHostToDevice);
+
+    hipsparseStatus_t stat = hipsparseSnnz(handle, dir, m, k, descrA, devA, m,
+                                         nnzPerRowColumnA, nnzA);
+    stat = hipsparseSnnz(handle, dir, k, n, descrB, devB, k,
+                         nnzPerRowColumnB, nnzB);
+    hipDeviceSynchronize();
+
+    int nnzA_h, nnzB_h;
+    hipMemcpy(&nnzA_h, nnzA, 1*sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy(&nnzB_h, nnzB, 1*sizeof(int), hipMemcpyDeviceToHost);
+
+    float *csrValA = NULL;
+    int *csrRowPtrA = NULL;
+    int *csrColIndA = NULL;
+    float *csrValB = NULL;
+    int *csrRowPtrB = NULL;
+    int *csrColIndB = NULL;
+    int *csrRowPtrC = NULL;
+    int *nnzC = NULL;
+
+    err = hipMalloc(&csrValA, sizeof(float)*nnzA_h);
+    err = hipMalloc(&csrRowPtrA, sizeof(int)*(m+1));
+    err = hipMalloc(&csrColIndA, sizeof(int)*nnzA_h);
+    err = hipMalloc(&csrValB, sizeof(float)*nnzB_h);
+    err = hipMalloc(&csrRowPtrB, sizeof(int)*(k+1));
+    err = hipMalloc(&csrColIndB, sizeof(int)*nnzB_h);
+    err = hipMalloc(&csrRowPtrC, sizeof(int)*(m+1));
+    err = hipMalloc(&nnzC, sizeof(int)*1);
+
+    stat = hipsparseSdense2csr(handle, m, k, descrA, devA, m, nnzPerRowColumnA,
+                                                 csrValA, csrRowPtrA, csrColIndA);
+
+    stat = hipsparseSdense2csr(handle, k, n, descrB, devB, k, nnzPerRowColumnB,
+                                                 csrValB, csrRowPtrB, csrColIndB);
+
+    stat = hipsparseXcsrgemmNnz(handle, HIPSPARSE_OPERATION_NON_TRANSPOSE,
+                                                  HIPSPARSE_OPERATION_NON_TRANSPOSE, m, n, k,
+                                                  descrA, nnzA_h, csrRowPtrA, csrColIndA,
+                                                  descrB, nnzB_h, csrRowPtrB, csrColIndB,
+                                                  descrC, csrRowPtrC, nnzC);
+
+    hipDeviceSynchronize();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int sum = 0;
+            for (int l = 0; l < k; l++)
+            {
+                sum += hostA[i*k + l] * hostB[j + l*n];
+            }
+            hostC[i*n + j] = sum;
+        }
+    }
+
+    float *devC = NULL;
+    int *nnzPerRowColumnC_h = NULL;
+    int *nnzC_h = NULL;
+    float *csrValC_h = NULL;
+    int *csrRowPtrC_h = NULL;
+    int *csrColIndC_h = NULL;
+
+    hipMalloc(&devC, sizeof(float)*m*n);
+    hipMemcpy(devC, hostC, m*n*sizeof(float), hipMemcpyHostToDevice);
+
+    err = hipMalloc(&nnzPerRowColumnC_h, sizeof(int)*m);
+    err = hipMalloc(&nnzC_h, sizeof(int) * 1);
+
+    stat = hipsparseSnnz(handle, dir, m, n, descrC, devC, m,
+                         nnzPerRowColumnC_h, nnzC_h);
+
+    int nnzC_ref_host;
+    hipMemcpy(&nnzC_ref_host, nnzC_h, 1*sizeof(int), hipMemcpyDeviceToHost);
+
+    err = hipMalloc(&csrValC_h, sizeof(float)*nnzC_ref_host);
+    err = hipMalloc(&csrRowPtrC_h, sizeof(int)*(m+1));
+    err = hipMalloc(&csrColIndC_h, sizeof(int)*nnzC_ref_host);
+
+    stat = hipsparseSdense2csr(handle, m, n, descrC, devC, m, nnzPerRowColumnC_h,
+                                                 csrValC_h, csrRowPtrC_h, csrColIndC_h);
+
+    int *csrRowPtrC_host = (int*) malloc ((m+1)*sizeof(int));
+    int *csrRowPtrC_ref_host = (int*) malloc ((m+1)*sizeof(int));
+    int nnzC_host;
+
+    hipMemcpy(csrRowPtrC_host, csrRowPtrC, (m+1)*sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy(csrRowPtrC_ref_host, csrRowPtrC_h, (m+1)*sizeof(int), hipMemcpyDeviceToHost);
+    hipMemcpy(&nnzC_host, nnzC, 1*sizeof(int), hipMemcpyDeviceToHost);
+
+    bool ispassed = 1;
+    for (int i = 0; i < m+1; i++) {
+      EXPECT_EQ(csrRowPtrC_ref_host[i], csrRowPtrC_host[i]);
+    }
+    
+    EXPECT_EQ(nnzC_ref_host, nnzC_host);
+ 
+    status1 = hipsparseDestroyMatDescr(descrA);
+    status1 = hipsparseDestroyMatDescr(descrB);
+    status1 = hipsparseDestroyMatDescr(descrC);
+    if (status1 != HIPSPARSE_STATUS_SUCCESS) {
+      exit(1);
+    }
+
+    status1 = hipsparseDestroy(handle);
+    if (status1 != HIPSPARSE_STATUS_SUCCESS) {
+      std::cout << "Error DeInitializing the sparse library."<<std::endl;
+      exit(1);
+    }
+   
+    hipFree(devA);
+    hipFree(devB);
+    hipFree(devC);
+    hipFree(nnzPerRowColumnA);
+    hipFree(nnzA);
+    hipFree(nnzPerRowColumnB);
+    hipFree(nnzB);
+    hipFree(csrValA);
+    hipFree(csrRowPtrA);
+    hipFree(csrColIndA);
+    hipFree(csrValB);
+    hipFree(csrRowPtrB);
+    hipFree(csrColIndB);
+    hipFree(csrRowPtrC);
+    hipFree(nnzC);
+    hipFree(nnzPerRowColumnC_h);
+    hipFree(nnzC_h);
+    hipFree(csrValC_h);
+    hipFree(csrRowPtrC_h);
+    hipFree(csrColIndC_h);
+    free(hostA);
+    free(hostB);
+    free(hostC);
+    free(csrRowPtrC_host);
+    free(csrRowPtrC_ref_host);
+}
