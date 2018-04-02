30715f439383edb8a0835ac8cd7972c02b6bebf3
Yuvaraj Venkatesh <yuvaraj@multicorewareinc.com>
2018-04-02 17:12:09 +0530

    revert commit 7bceeb77bff305a67696345f288207c1d271c2cc

--- lib/src/transform/hcsparse-dense2csc.h
+++ lib/src/transform/hcsparse-dense2csc.h
@@ -1,5 +1,29 @@
 #include "hcsparse.h"
 
+#define THREADS 16
+
+template <typename T>
+void transpose_kernel (hcsparseControl* control,
+                       int rows,
+                       int cols,
+                       const T* A,
+                       T* transA)
+{
+   hc::extent<2> grdExt((rows + THREADS-1) & ~(THREADS-1), (cols + THREADS-1) & ~(THREADS-1));
+   hc::tiled_extent<2> t_ext = grdExt.tile(THREADS, THREADS);
+   hc::parallel_for_each(control->accl_view, t_ext, [ = ] (hc::tiled_index<2>& tidx) [[hc]] {
+     int gidx = tidx.global[1];
+     int gidy = tidx.global[0];
+
+     if (gidx < cols && gidy < rows) {
+       unsigned int index_in = gidy * cols + gidx;
+       unsigned int index_out = gidx * rows + gidy;
+
+       transA[index_in] = A[index_out];
+     }
+   });
+}
+
 template <typename T>
 hcsparseStatus
 dense2csc (hcsparseControl* control,
@@ -12,27 +36,16 @@ dense2csc (hcsparseControl* control,
 
   hc::accelerator acc = (control->accl_view).get_accelerator();
 
-  hc::extent<1> grdExt(1);
-  hc::tiled_extent<1> t_ext = grdExt.tile(1);
-  hc::parallel_for_each(control->accl_view, t_ext, [=] (hc::tiled_index<1> &tidx) [[hc]]
-  {
-      cscColPtrA[0] = 0;
-      int count = 0, col_off = 0;
-      for (int i = 0; i < n; i++)
-      {
-          for (int j = 0; j < m; j++)
-          {
-              if (A[j*n + i] != 0)
-              {
-                  cscValA[count] = A[j*n+i];
-                  cscRowIndA[count] = j;
-                  col_off++;
-                  count++;
-              }
-          }
-          cscColPtrA[i+1] = col_off;
-      }
-  });
+  ulong dense_size = m * n;
+  T* transA = (T*)am_alloc(dense_size * sizeof (T), acc, 0);
+
+  // Transpose the matrix so it will be in row major format
+  transpose_kernel<T>(control, m, n, A, transA);
+
+  // Perform dense2csr conversion
+  dense2csr(control, n, m, transA, cscValA, cscColPtrA, cscRowIndA);
+
+  am_free(transA);
 
   return hcsparseSuccess;
 }
